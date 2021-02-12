19f785393bc37b0d50f0d696f75c0ed9f4da1754
quardz <adal@quardz.com>
2021-02-12 03:34:27 -0900

    Added round 1 and round 2 with soft/hard cap

--- contracts/FontsCrowdsale.sol
+++ contracts/FontsCrowdsale.sol
@@ -11,20 +11,19 @@ interface Pauseable {
 }
 
 /**
- * @title TacosCrowdsale
- * @dev Crowdsale contract for $TACO.
+ * @title FontsCrowdsale
+ * @dev Crowdsale contract for $FONT.
  *      Pre-Sale done in this manner:
- *        1st Round: Early Cooks (2 ETH contribution max during round, CAP 70 ETH)
- *        2nd Round: $KARMA holders (2 ETH contribution max during round, CAP 70 ETH)
- *        3rd Round: Public Sale (2 ETH contribution max during round, CAP 70 ETH)
- *        - Any single address can contribute at most 2 ETH -
- *      1 ETH = 20000 $TACO (during the entire sale)
- *      Hardcap = 210 ETH
+ *        1st Round: 180ETH and 1111 $FONT per $ETH
+ *        2nd Round: 220ETH and 909 $FONT per $ETH
+ *      Softcap = 300 ETH
+ *      Hardcap = 400 ETH
  *      Once hardcap is reached:
  *        All liquidity is added to Uniswap and locked automatically, 0% risk of rug pull.
  *
  * @author soulbar@protonmail.com ($TEND)
  * @author @Onchained ($TACO)
+ * @author @adalquardz ($FONT)
  */
 contract FontsCrowdsale is Ownable {
     using SafeMath for uint256;
@@ -35,39 +34,38 @@ contract FontsCrowdsale is Ownable {
     //          Contract Variables                   //
     //===============================================//
 
+
     // Caps
-    uint256 public constant ROUND_1_CAP = 175 ether; // CAP = 175
-    uint256 public constant ROUND_2_CAP = 395 ether; // CAP = 220
-    //#uint256 public constant ROUND_3_CAP = 210 ether; // CAP = 70
-    // HARDCAP = ROUND_3_CAP
+    uint256 public constant ROUND_1_CAP = 180 ether; 
+    uint256 public constant ROUND_2_CAP = 400 ether; 
 
+    uint256 public constant SOFT_CAP = 300 ether; // Softcap  = 300
+    uint256 public constant HARD_CAP = 400 ether; // hardcap = +100
 
+    uint256 public constant FONT_PER_ETH_ROUND_1 = 1111;
+    uint256 public constant FONT_PER_ETH_ROUND_2 = 909;
+    
     
     // During tests, we should use 12 ether instead given that by default we only have 20 addresses.
     uint256 public constant CAP_PER_ADDRESS = 6 ether;
     uint256 public constant MIN_CONTRIBUTION = 0.1 ether;
 
+
+    //TIME
     // Start time 08/09/2020 @ 6:00am (UTC) // For Cooks
     uint256 public constant CROWDSALE_START_TIME = 1596952800;
-
-
     // Start time 08/11/2020 @ 4:00pm (UTC)
-    uint256 public constant PUBLICSALE_START_TIME = 1597161600;
 
     // End time
-    uint256 public constant CROWDSALE_END_TIME = PUBLICSALE_START_TIME + 1 days;
+    uint256 public constant CROWDSALE_END_TIME = 7 days;
 
 
     // How many ETH need to lock in Uniswap LP 
-    uint256 amountEthForUniswap = 144 ether;
-    
+    uint256 AMOUNT_ETH_FOR_UNISWAP_LP = 143 ether;
+  
     // How many $FONTs need to lock in Uniswap LP 
-    uint256 amountTokensForUniswap = fontToken.balanceOf(address(this));
+    uint256 AMOUNT_FONT_FOR_UNISWAP_LP = 100000;;
 
-
-    // Early cooks list for round 1
-    // Too many cooks? https://www.youtube.com/watch?v=QrGrOK8oZG8
-    mapping(address => bool) public cookslist;
 
     // Contributions state
     mapping(address => uint256) public contributions;
@@ -78,15 +76,10 @@ contract FontsCrowdsale is Ownable {
     // Flag to know if liquidity has been locked
     bool public liquidityLocked = false;
 
-    // Pointer to the TacoToken
+    // Pointer to the FONTToken
     IERC20 public fontToken;
 
 
-    // How many tacos do we send per ETH contributed in round 1
-    uint256 public fontsPerEth_round_1;
-
-    // How many tacos do we send per ETH contributed in round 2
-    uint256 public fontsPerEth_round_2;    
 
     // Pointer to the UniswapRouter
     IUniswapV2Router02 internal uniswapRouter;
@@ -96,14 +89,10 @@ contract FontsCrowdsale is Ownable {
     //===============================================//
     constructor(
         IERC20 _fontToken,
-        uint256 _fontsPerEth_round_1,
-        uint256 _fontsPerEth_round_2,
         address _uniswapRouter
     ) public Ownable() {
         owner = msg.sender;        
         fontToken = _fontToken;
-        fontsPerEth_round_1 = _fontsPerEth_round_1;
-        fontsPerEth_round_2 = _fontsPerEth_round_2;
         uniswapRouter = IUniswapV2Router02(_uniswapRouter);
     }
 
@@ -133,15 +122,11 @@ contract FontsCrowdsale is Ownable {
                 "FontsCrowdsale: The cap for the current round has been filled."
             );
             require(
-                _allowedInCurrentRound(msg.sender),
-                "TacosCrowdsale: Address not allowed for this round."
-            );
-            require(
                 contributions[msg.sender] < CAP_PER_ADDRESS,
-                "TacosCrowdsale: Individual cap has been filled."
+                "FontsCrowdsale: Individual cap has been filled."
             );
 
-            // If we've passed most validations, let's get them $TACOs
+            // If we've passed most validations, let's get them $FONTs
             _buyTokens(msg.sender);
         }
     }
@@ -182,12 +167,12 @@ contract FontsCrowdsale is Ownable {
     }
 
     /**
-     * Function that validates the minimum wei amount, then perform the actual transfer of $TACOs
+     * Function that validates the minimum wei amount, then perform the actual transfer of $FONTs
      */
     function _buyTokens(address beneficiary, uint256 weiAmount, uint256 weiAllowanceForRound) internal {
         require(
             weiAmount >= MIN_CONTRIBUTION || weiAllowanceForRound < MIN_CONTRIBUTION,
-            "TacosCrowdsale: weiAmount is smaller than min contribution."
+            "FontsCrowdsale: weiAmount is smaller than min contribution."
         );
 
         // Update how much wei we have raised
@@ -195,66 +180,54 @@ contract FontsCrowdsale is Ownable {
         // Update how much wei has this address contributed
         contributions[beneficiary] = contributions[beneficiary].add(weiAmount);
 
-        // Calculate how many $TACOs can be bought with that wei amount
+        // Calculate how many $FONTs can be bought with that wei amount
         uint256 tokenAmount = _getTokenAmount(weiAmount);
-        // Transfer the $TACOs to the beneficiary
+        // Transfer the $FONTs to the beneficiary
         fontToken.safeTransfer(beneficiary, tokenAmount);
 
         // Create an event for this purchase
         emit TokenPurchase(beneficiary, weiAmount, tokenAmount);
     }
 
-    // Calculate how many tacos do they get given the amount of wei
-    function _getTokenAmount(uint256 weiAmount) internal view returns (uint256)
-    {
-        return weiAmount.mul(tacosPerEth);
+    // Calculate how many fonts do they get given the amount of wei
+    //@done
+    function _getTokenAmount(uint256 weiAmount) internal view returns (uint256){
+        return weiAmount.mul(_getFontsPerETH());
     }
 
     // CONTROL FUNCTIONS
 
     // Is the sale open now?
+    //@done
     function isOpen() public view returns (bool) {
         return now >= CROWDSALE_START_TIME;
     }
 
     // Has the sale ended?
+    //@done
     function hasEnded() public view returns (bool) {
         return now >= CROWDSALE_END_TIME || weiRaised >= ROUND_2_CAP;
     }
 
-    // Has the *Karma* sale started?
-    function karmaSaleStarted() public view returns (bool) {
-        return now >= KARMASALE_START_TIME;
+    // Has the Round 2 started
+    //@done
+    function roundTwoStarted() public view returns (bool) {
+        return weiRaised >= ROUND_1_CAP;
     }
 
-    // Has the *Public* sale started?
-    function publicSaleStarted() public view returns (bool) {
-        return now >= PUBLICSALE_START_TIME;
-    }
-
-    // Is the beneficiary allowed in the current Round?
-    function _allowedInCurrentRound(address beneficiary) internal view returns (bool) {
-        bool isKarmaRoundAndMember = karmaSaleStarted() && _isKarmaMember(beneficiary);
-
-        return (cookslist[beneficiary] || isKarmaRoundAndMember || publicSaleStarted());
-    }
-
-    // Checks wether the beneficiary is a Karma member.
-    // Karma membership is defined as owning 200 $KARMA
-    // Thank you KarmaDAO for getting us together.
-    function _isKarmaMember(address beneficiary) internal view returns (bool) {
-        return karmaToken.balanceOf(beneficiary) >= KARMA_MEMBERSHIP_AMOUNT;
-    }
-
+    //@done
     function _getFontsPerETH() internal view returns (uint256) {
-
+        if (roundTwoStarted()) {
+            return FONT_PER_ETH_ROUND_2;
+        } else { // Cooks sale
+            return FONT_PER_ETH_ROUND_1;
+        }
     }
 
     // What's the total cap for the current round?
+    //@done
     function _totalCapForCurrentRound() internal view returns (uint256) {
-        if (publicSaleStarted()) {
-            return ROUND_3_CAP;
-        } else if(karmaSaleStarted()) {
+        if (roundTwoStarted()) {
             return ROUND_2_CAP;
         } else { // Cooks sale
             return ROUND_1_CAP;
@@ -262,32 +235,26 @@ contract FontsCrowdsale is Ownable {
     }
 
     // Return human-readable currentRound
+    //@done
     function getCurrentRound() public view returns (string memory) {
-        if (publicSaleStarted()) return "Public";
-        if (karmaSaleStarted()) return "Karma";
-        return "Cooks";
+        if (roundTwoStarted()) return "Round 2";
+        return "Round 1";
     }
 
-    // Set the cooks list
-    function setCooksList(address[] calldata accounts) external onlyOwner {
-        for (uint256 i = 0; i < accounts.length; i++) {
-            cookslist[accounts[i]] = true;
-        }
-    }
 
     /**
      * Function that once sale is complete add the liquidity to Uniswap
      * then locks the liquidity by burning the UNI tokens.
      * 
-     * Originally this function was public, however this was abused in $TACO.
+     * Originally this function was public, however this was abused in $FONT.
      * Somebody made a contract that deployed the liquidity and bought 120ETH
-     * worth of $TACO within the same transaction, essentially defeating the
+     * worth of $FONT within the same transaction, essentially defeating the
      * purpose of trying to prevent whales from joining in.
      * https://etherscan.io/tx/0xeb84f538c0afeb0b542311236be10529925bd80e5fb34e4e24256b8b456234da/
      *
      * A few things to prevent that have been discussed.
      * 1. Make this function onlyOwner
-     * 2. After liquidity is added, when tacoToken is unpaused and for 24 hours
+     * 2. After liquidity is added, when FONTToken is unpaused and for 24 hours
      *      after that, no single transaction can be done for more than X amount.
      *      Forcing whales to execute more than a single transaction.
      * 3. A Bridge Tax or just a sale tax. Everytime a whale sells their tokens
@@ -298,53 +265,64 @@ contract FontsCrowdsale is Ownable {
      */
     function addAndLockLiquidity() external onlyOwner {
         require(
-            hasEnded(),
-            "TacosCrowdsale: can only send liquidity once hardcap is reached"
+            isCrowdsaleSuccess(),
+            "FontCrowdsale: can only send liquidity once softcap is reached"
         );
+        require(!liquidityLocked, "FontCrowdsale: Liquidity already locked");
 
-
-        // Unpause TacoToken forever. This will kick off the game.
+        // Unpause FONTToken forever. This will kick off the game.
         Pauseable(address(fontToken)).unpause();
 
         // Send the entire balance and all tokens in the contract to Uniswap LP
-        fontToken.approve(address(uniswapRouter), amountTokensForUniswap);
-        uniswapRouter.addLiquidityETH{value: amountEthForUniswap}(
+        fontToken.approve(address(uniswapRouter), AMOUNT_FONT_FOR_UNISWAP_LP);
+
+        uniswapRouter.addLiquidityETH{value: AMOUNT_ETH_FOR_UNISWAP_LP}(
             address(fontToken),
-            amountTokensForUniswap,
-            amountTokensForUniswap,
-            amountEthForUniswap,
+            AMOUNT_FONT_FOR_UNISWAP_LP,
+            AMOUNT_FONT_FOR_UNISWAP_LP,
+            AMOUNT_ETH_FOR_UNISWAP_LP,
             address(0), // burn address
             now
         );
         liquidityLocked = true;
     }
 
+    // Return bool when crowdsale reached soft cap and time is over
+    //@Done
+    function isCrowdsaleSuccess() public view returns (bool) {
+        return now >= CROWDSALE_END_TIME && weiRaised >= SOFT_CAP;
+    }    
+    //@Done
+    function isCrowdsaleEnded() public view returns (bool) {
+        return now >= CROWDSALE_END_TIME;
+    }
+    //@Done
     function withdrawEth(uint amount) external onlyOwner returns(bool){
         require(
-            hasEnded(),
-            "TacosCrowdsale: can only send liquidity once hardcap is reached"
+            isCrowdsaleSuccess(),
+            "FontsCrowdsale: can only send liquidity once hardcap is reached"
         );        
-        require(amount <= this.balance);
         require(liquidityLocked, "FontCrowdsale: Can't withdraw before Uniswap liquidity locked");
+        require(amount <= this.balance);
         owner.transfer(amount);
         return true;
     }
-
+    //@Done
     function withdrawFonts(uint amount) external onlyOwner returns(bool){
         require(
-            hasEnded(),
-            "FontCrowdsale: can only send liquidity once hardcap is reached"
+            isCrowdsaleEnded(),
+            "FontCrowdsale: can only withdraw after crowdsale time ends"
         );  
         require(liquidityLocked, "FontCrowdsale: Can't withdraw before Uniswap liquidity locked");
         require(amount <= fontToken.balanceOf(address(this)));
         fontToken.safeTransfer(owner, amount);
         return true;
     }
-
+    //@Done
     function getFontBalance() public view returns(uint){
         return fontToken.balanceOf(address(this));
     }
-
+    //@Done 
     function getEthBalance() public view returns(uint){
         return this.balance;
     }
---
+++ contracts/readme.txt
@@ -0,0 +1,9 @@
+Soft cap 300
+HArdcap 400
+
+List if softcap reached
+REfun if soft cap not reaced 
+
+Stop if time exired, or hard cap reached
+
+Withdraw only if soft cap reached and time exipred 
